1bbaac96e51c78d4601b412300f0396829997ab8
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-09-02 08:53:41 +0900

    feat: throughputを出す

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,33 +1,46 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-
 const BATCH_SIZE: usize = 1024;
 
-fn push_benchmark(c: &mut Criterion) {
-    c.bench_function("push", |b| {
-        b.iter(|| {
-            let mut vec = Vec::with_capacity(BATCH_SIZE);
-            for i in 0..BATCH_SIZE {
-                vec.push(black_box(i));
-            }
-        })
-    });
+fn benchmark(c: &mut Criterion) {
+    let mut g = c.benchmark_group("Vec Insertion Methods");
+    for size_b in 10..=21 {
+        let size = 1 << size_b;
+
+        g.throughput(criterion::Throughput::Elements(size as u64));
+        g.bench_with_input(
+            criterion::BenchmarkId::new("push", size),
+            &size,
+            |b, &size| {
+                b.iter(|| {
+                    let mut vec = Vec::with_capacity(size);
+                    for i in 0..size {
+                        vec.push(black_box(i));
+                    }
+                    black_box(vec);
+                });
+            },
+        );
+        g.bench_with_input(
+            criterion::BenchmarkId::new("spare_capacity_mut", size),
+            &size,
+            |b, &size| {
+                b.iter(|| {
+                    let mut vec = Vec::with_capacity(size);
+                    unsafe {
+                        let ptr = vec.spare_capacity_mut();
+                        for i in 0..size {
+                            ptr[i].write(black_box(i));
+                        }
+                        vec.set_len(size);
+                    }
+                    black_box(vec);
+                });
+            },
+        );
+    }
+    g.finish();
 }
 
-fn spare_capacity_mut_benchmark(c: &mut Criterion) {
-    c.bench_function("spare_capacity_mut", |b| {
-        b.iter(|| {
-            let mut vec = Vec::with_capacity(BATCH_SIZE);
-            let spare = vec.spare_capacity_mut();
-            for i in 0..BATCH_SIZE {
-                spare[i].write(black_box(i));
-            }
-            unsafe {
-                vec.set_len(BATCH_SIZE);
-            }
-        })
-    });
-}
-
-criterion_group!(benches, push_benchmark, spare_capacity_mut_benchmark);
+criterion_group!(benches, benchmark);
 criterion_main!(benches);
